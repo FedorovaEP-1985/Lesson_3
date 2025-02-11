fe825a269048392970d48b0e8891deb06bf1a285
ЕЛЕНА <lenok_zvereva@mail.ru>
2025-02-11 15:28:10 +0300

    Initial commit

---
+++ lesson_3.py
@@ -0,0 +1,19 @@
+from user import User
+from card import Card
+
+
+Helen = User("Lena")
+#Helen.sayname()
+#Helen.sayage()
+#Helen.setage(40)
+#Helen.sayage()
+Helen.firstname()
+
+
+
+
+
+#card = Card("2202 2000 4565 1264", "09/34", "Helen F")
+#card.pay(3000)
+
+
---
+++ user.py
@@ -0,0 +1,32 @@
+class User:
+    age = 39;
+    firstname = ("Lena")
+    lastname = ("Fedorova")
+
+    #def __init__(self, name):
+        #print("я создался")
+        #self.username = name
+
+    #def sayname(self):
+        #print("меня зовут ", self.username)
+
+    #def sayage(self):
+        #print(self.age)
+
+    #def setage(self, newAge):
+        #self.age = newAge
+
+
+    def __init__(self, name):
+        print("я тут")
+        self.username = name
+
+        #self.lastname = lastname
+
+    def firstname(self):
+            print("меня зовут ", self.username)
+
+    def SONAME(self):
+            print("меня зовут ", self, firstname, lastname)
+            self.firstname = username
+            self.lastname = ()

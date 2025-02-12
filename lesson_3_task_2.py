d1894be581206bcb4ba6d8d091680af7cc9d0925
FedorovaEP-1985 <lenok_zvereva@mail.ru>
2025-02-12 16:00:10 +0300

    Initial commit

---
+++ lesson_3_task_2.py
@@ -0,0 +1,17 @@
+from user import User
+from Smartphone import Smart
+
+
+Helen = User("Lena")
+Helen.first_name()
+Helen.abonent()
+
+
+smartphone_1 = Smart (["Nokia"], ["2210"], ["+79152268093"], ["Danya"])
+smartphone_2 = Smart (["Fly"], ["1111"], ["+79222222222"], ["Oly"])
+smartphone_3 = Smart (["Motorola"], ["A4"], ["+79111111111"], ["Dima"])
+
+#Smart_1.class_attribute = "Smartphone"
+#print(smart_1.class_attribute)
+
+smartphone_1.calls ("12:00")

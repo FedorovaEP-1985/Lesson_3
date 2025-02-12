224601db114ced31246dd08076fc38bf29809446
FedorovaEP-1985 <lenok_zvereva@mail.ru>
2025-02-12 19:08:55 +0300

    Initial commit

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -1,11 +1,11 @@
 from Smartphone import Smart
 
 catalog = []
-catalog.append(Smart ("Nokia", "2210", "+79152268093", "Danya"))
-catalog.append(Smart ("Fly", "1111", "+79222222222", "Oly"))
+catalog.append(Smart("Nokia", "2210", "+79152268093", "Danya"))
+catalog.append(Smart("Fly", "1111", "+79222222222", "Oly"))
 catalog.append(Smart("Motorola", "A4", "+79111111111", "Dima"))
-catalog.append(Smart ("Samsung", "Galaxy11", "+7-999-123-45-67", "Dily"))
-catalog.append(Smart ("IPhone", "13 ProMax", "+7-999-698-85-96", "Lena"))
+catalog.append(Smart("Samsung", "Galaxy11", "+7-999-123-45-67", "Dily"))
+catalog.append(Smart("IPhone", "13 ProMax", "+7-999-698-85-96", "Lena"))
 
 
 #catalog.append = (Smart ("Nokia", "2210", "+79152268093", "Danya"))
@@ -18,7 +18,7 @@ catalog.append(Smart ("IPhone", "13 ProMax", "+7-999-698-85-96", "Lena"))
 #phone.calls ("12:00")
 
 for smart in catalog:
-        print(f"{phone_abonent}'s {phone.brand} {phone.model}: {phone.number}")
+        print(f"(phone_abonent0, (phone.brand), (phone.model): (phone.number)")
 
 
 

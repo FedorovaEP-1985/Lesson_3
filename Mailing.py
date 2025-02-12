bd16c0152831352403cb642f110634ac923c2fb5
FedorovaEP-1985 <lenok_zvereva@mail.ru>
2025-02-12 19:35:17 +0300

    Initial commit

--- Mailing.py
+++ Mailing.py
@@ -1,12 +1,12 @@
 class Mailing:
-    def __init__(self, to_address, from_address, weight, tracking_id):
-        self.to_address = to_address
-        self.from_address = from_address
+    def __init__(self, to_adress, from_adress, weight, tracking_id):
+        self.to_adress = to_adress
+        self.from_adress = from_adress
         self.weight = weight
         self.tracking_id = tracking_id
 
     def __repr__(self) -> str:
-        return f'Отправление: вес {self.weight}, трекинг-код {self.tracking_id}\nОткуда: {self.from_address}\nКуда: {self.to_address}'
+        return f'Отправление: вес {self.weight}, трекинг-код {self.tracking_id}\nОткуда: {self.from_adress}\nКуда: {self.to_adress}'
 
    # def __str__(self):
     #return (f" отправление {self.trak} из {self.from_adress}"

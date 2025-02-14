3531461567e10a7fa2870b7f6ac42c3723e219b1
FedorovaEP-1985 <lenok_zvereva@mail.ru>
2025-02-14 15:25:20 +0300

    обновление

--- Smartphone.py
+++ Smartphone.py
@@ -1,19 +1,13 @@
 class Smart:
-	number = 'number'
-	model_phone = 'model'
-	phone_brand = 'brand'
-	owner = "owner"
+    def __init__(self, brand, model, number, owner):
+        self.number = number
+        self.model = model
+        self.brand = brand
+        self.owner = owner
 
-	class Smart:
-		def __init__(self, brand, model, number, owner):
-			self.number = number
-			self.model = model
-			self.brand = brand
-			self.owner = owner
+    def __repr__(self):
+        return f'Smart({self.brand}, {self.model}, {self.number}, {self.owner})'
 
-def __repr__str(self):
-    return f'Smart({self.phone_brand}, {self.model_phone}, {self.number}, {self.owner})'
- 
 #def calls(self, kuki):
 	#print("Звонил", self.number, "время", kuki )
 
--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -7,18 +7,10 @@ catalog.append(Smart("Motorola", "A4", "+79111111111", "Dima"))
 catalog.append(Smart("Samsung", "Galaxy11", "+7-999-123-45-67", "Dily"))
 catalog.append(Smart("IPhone", "13 ProMax", "+7-999-698-85-96", "Lena"))
 
-
-#catalog.append = (Smart ("Nokia", "2210", "+79152268093", "Danya"))
-#phone  = Smart ("Fly", "1111", "+79222222222", "Oly")
-#phone  = Smart ("Motorola", "A4", "+79111111111", "Dima")
-#phone  = Smart ("Samsung", "Galaxy11", "+7-999-123-45-67", "Dily")
-#phone  = Smart ("IPhone", "13 ProMax", "+7-999-698-85-96", "Lena")
-
-
 #phone.calls ("12:00")
 
 for smart in catalog:
-        print(f"{smart.owner}, {smart.brand}, {smart.model}: {smart.number}")
+    print(f"{smart.owner}, {smart.brand}, {smart.model}: {smart.number}")
 
 
 
